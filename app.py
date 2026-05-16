63ef95b44a3d40b349ae4f967962127e5ce8fbfa
ashabakshi <ashabakshi13@gmail.com>
2026-05-16 21:45:16 +0530

    update

--- app.py
+++ app.py
@@ -559,7 +559,7 @@ st.markdown("""
 # ---------------- LOAD FILES (cached) ----------------
 @st.cache_resource
 def load_assets():
-    model = load_model("lstm_model.h5", compile=False)
+    model = load_model("lstm_model.h5", compile=False,safe_mode=False)
     with open("tokenizer.pkl", "rb") as f:
         tokenizer = pickle.load(f)
     with open("max_len.pkl", "rb") as f:
